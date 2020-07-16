22782e95462aa98db44ab61008eb30fd70632bda
Adrian Ehrsam <ehrsam@kull.ch>
2020-07-16 06:25:42 +0200

    Better config

--- Kull.GenericBackend/Execution/CommandPreparation.cs
+++ Kull.GenericBackend/Execution/CommandPreparation.cs
@@ -25,14 +25,23 @@ namespace Kull.GenericBackend.Execution
         private readonly ParameterProvider parameterProvider;
         private readonly SPParametersProvider sPParametersProvider;
 
-        public CommandPreparation(
-            ParameterProvider parameterProvider,
-            SPParametersProvider sPParametersProvider)
+        public CommandPreparation(IServiceProvider serviceProvider) // Use of IServiceProvider to not break inheritance
         {
-            this.parameterProvider = parameterProvider;
-            this.sPParametersProvider = sPParametersProvider;
+            this.parameterProvider = (ParameterProvider)serviceProvider.GetService(typeof(ParameterProvider));
+            this.sPParametersProvider = (SPParametersProvider)serviceProvider.GetService(typeof(SPParametersProvider));
         }
-        public DbCommand GetCommandWithParameters(HttpContext? context,
+
+        /// <summary>
+        /// Gets the command with parameters
+        /// </summary>
+        /// <param name="context">The http context or null if not in an http context</param>
+        /// <param name="getRouteValue">If context is null, you need to provider route parameters with this Func</param>
+        /// <param name="con">The db connection</param>
+        /// <param name="ent">The entity</param>
+        /// <param name="method">The method</param>
+        /// <param name="parameterOfUser">The parameters as provided from user</param>
+        /// <returns>Returns a DbCommand Object, NOT executed</returns>
+        public virtual DbCommand GetCommandWithParameters(HttpContext? context,
                 Func<string, object>? getRouteValue,
                 DbConnection con,
                 Entity ent,
@@ -96,8 +105,10 @@ namespace Kull.GenericBackend.Execution
                     sPParameters = sPParameters ?? sPParametersProvider.GetSPParameters(method.SP, con);
                     var spPrm = sPParameters.First(f => f.SqlName == apiPrm.SqlName);
                     if (spPrm.DbType.NetType == typeof(System.DateTime)
+                        || spPrm.DbType.NetType == typeof(System.DateTimeOffset)
                         || spPrm.DbType.JsType == "number"
-                         || spPrm.DbType.JsType == "integer")
+                        || spPrm.DbType.JsType == "integer"
+                        || spPrm.DbType.JsType == "boolean")
                     {
                         cmd.AddCommandParameter(apiPrm.SqlName, DBNull.Value);
                     }
